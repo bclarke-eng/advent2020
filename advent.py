50c1c4e7d65c4fb04460a4e6ab099f59701fd4f4
bcprobert <55083713+bcprobert@users.noreply.github.com>
2020-12-01 15:02:35 +0000

    Update advent.py

--- advent.py
+++ advent.py
@@ -7,4 +7,3 @@ print([x * y for i, x in enumerate(num_list) for j, y in enumerate(num_list) if 
 
 # Finds three different numbers in the data that add to 2020 and prints their product
 print([x * y * z for i, x in enumerate(num_list) for j, y in enumerate(num_list) for k, z in enumerate(num_list) if (i != j != k and x + y + z == 2020)][0])
-git
