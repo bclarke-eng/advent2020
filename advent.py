2213e328bf4cee469414e51dc4e15b8b1bbfab23
PROBERT Bethany <bethany.probert@altran.com>
2020-12-02 10:11:52 +0000

    completed day 2 part 1

--- advent.py
+++ advent.py
@@ -1,9 +1,35 @@
+import re
+
+# DAY 1
+
 # Reads in the data and converts it to a list of numbers
-with open("data.txt", "r") as f:
+with open("data1.txt", "r") as f:
     num_list = [int(number) for number in f.read().split()]
 
 # Finds two different numbers in the data that add to 2020 and prints their product
-print([x * y for i, x in enumerate(num_list) for j, y in enumerate(num_list) if (i != j and x + y == 2020)][0])
+print("Multiple for 2 numbers:", [x * y for i, x in enumerate(num_list) for j, y in enumerate(num_list) if (i != j and x + y == 2020)][0])
 
 # Finds three different numbers in the data that add to 2020 and prints their product
-print([x * y * z for i, x in enumerate(num_list) for j, y in enumerate(num_list) for k, z in enumerate(num_list) if (i != j != k and x + y + z == 2020)][0])
+print("Multiple for 3 numbers:", [x * y * z for i, x in enumerate(num_list) for j, y in enumerate(num_list) for k, z in enumerate(num_list) if (i != j != k and x + y + z == 2020)][0])
+# -------------------------------------------------------------------------------------------------------
+
+# DAY 2
+
+# Reads in the data and converts it to a list of passwords with policies
+with open("data2.txt", "r") as g:
+    password_list = [password for password in g.read().splitlines()]
+
+valid_passwords = 0
+
+for word in password_list:
+    # Regex search to find needed information
+    list_item = re.findall(r"^(\d{1,})(-)(\d{1,})( )(\w)(: )(\w+)$", word)[0]
+    policy_min_number = int(list_item[0])
+    policy_max_number = int(list_item[2])
+    policy_letter = list_item[4]
+    pass_word = list_item[6]
+
+    if policy_min_number <= pass_word.count(policy_letter) <= policy_max_number:
+        valid_passwords += 1
+
+print("Number of valid passwords:", valid_passwords)
